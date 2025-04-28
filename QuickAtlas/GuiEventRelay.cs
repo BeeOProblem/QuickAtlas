bd27889e0f81435df3bc9daed525a90c007cd596
BeeOProblem <ds2001man@msn.com>
2025-04-27 23:53:31 -0400

    Ensure relayed input uses proper relative mouse posision

--- QuickAtlas/GuiEventRelay.cs
+++ QuickAtlas/GuiEventRelay.cs
@@ -10,6 +10,14 @@ public partial class GuiEventRelay : MarginContainer
 
     public override void _GuiInput(InputEvent inputEvent)
     {
+        // mouse events need position modified since _GuiInput assumes the position
+        // is relative to the control receiving the event and will behave weird if
+        // that assumption is broken by the position being relative to some other control
+        if (inputEvent is InputEventMouse mouse) {
+            var offset = RelayTarget.GlobalPosition - GlobalPosition;
+            mouse.Position -= offset;
+        }
+
         RelayTarget._GuiInput(inputEvent);
     }
 }
